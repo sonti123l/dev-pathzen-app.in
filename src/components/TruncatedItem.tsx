607e4eb9b0da3ced3dc43a8c203dd39ac736ccf3
SaiTrishal22 <saitrishal.s@orotron.com>
2026-04-13 14:52:11 +0530

    fixing the layout and sidebar

--- src/components/TruncatedItem.tsx
+++ src/components/TruncatedItem.tsx
@@ -1,29 +1,29 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "~/components/ui/tooltip";
+  import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "~/components/ui/tooltip";
 
-const TruncatedItem = ({ name }: { name: string }) => {
-  const MAX = 25;
-  const isTruncated = name.length > MAX;
-  const displayName = isTruncated ? name.slice(0, MAX) + "..." : name;
+  const TruncatedItem = ({ name }: { name: string }) => {
+    const MAX = 25;
+    const isTruncated = name.length > MAX;
+    const displayName = isTruncated ? name.slice(0, MAX) + "..." : name;
 
-  if (!isTruncated) return <span>{name}</span>;
+    if (!isTruncated) return <span>{name}</span>;
 
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <span>{displayName}</span>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{name}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  );
-};
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span>{displayName}</span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>{name}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  };
 
-export default TruncatedItem;
+  export default TruncatedItem;
