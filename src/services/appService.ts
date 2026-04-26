dc1acc6a3c5fd1f6d5b819da3112e228351e3e56
SaiTrishal22 <saitrishal.s@orotron.com>
2026-04-26 21:25:52 +0530

    course dashboard course video tommorrow needs to complete the teacher student total live streaming

--- src/services/appService.ts
+++ src/services/appService.ts
@@ -2,7 +2,7 @@ import { $fetch } from "~/http/fetch";
 
 export const scheduleMeeting = async (
   id: number,
-  payload: { live_time: string },
+  payload: { live_time: string, live_date: string },
 ) => {
   try {
     const res = await $fetch.patch(`/course/submodule/${id}/settime`, payload);
